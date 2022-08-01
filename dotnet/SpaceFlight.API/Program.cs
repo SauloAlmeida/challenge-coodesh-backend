a38b7d6c142bedcf8d3022689e8c3b6ecdb406f7
Saulo Almeida <saulohenrique786@gmail.com>
2022-08-01 12:39:01 -0300

    feat: update article

--- dotnet/SpaceFlight.API/Program.cs
+++ dotnet/SpaceFlight.API/Program.cs
@@ -4,6 +4,7 @@ using SpaceFlight.API.Application.DTO.ViewModel;
 using SpaceFlight.API.Application.Model;
 using SpaceFlight.API.Core.Contracts.Infrastructure;
 using SpaceFlight.API.Setup;
+using System.ComponentModel;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +25,7 @@ app.MapGet("/", () => Results.Ok("Back-end Challenge 2021 - Space Flight News"))
 app.MapGet("/articles", async (IDatabase db, CancellationToken token)
     => await db.Collection.Find(_ => true).ToListAsync(token));
 
-app.MapGet("/articles/{id}", async (long id, IDatabase db, CancellationToken token)
+app.MapGet("/articles/{id}", async (int id, IDatabase db, CancellationToken token)
     => await db.Collection.Find(f => f.Id == id).SingleOrDefaultAsync(token));
 
 app.MapPost("/articles", async ([FromBody] ArticleDTO dto, IDatabase db, CancellationToken token) =>
@@ -36,6 +37,15 @@ app.MapPost("/articles", async ([FromBody] ArticleDTO dto, IDatabase db, Cancell
     await db.Collection.InsertOneAsync(entity, cancellationToken: token);
 
     Results.Created($"/articles/{entity.Id}", entity);
+});
+
+app.MapPut("/articles/{id}", async (int id, [FromBody] ArticleDTO dto, IDatabase db, CancellationToken token) =>
+{
+    var filter = Builders<Article>.Filter.Eq(p => p.Id, id);
+
+    await db.Collection.ReplaceOneAsync(filter, dto.ToEntity(id), cancellationToken: token);
+
+    Results.NoContent();
 });
 
 app.MapDelete("/articles/{id}", async (long id, IDatabase db, CancellationToken token) =>
