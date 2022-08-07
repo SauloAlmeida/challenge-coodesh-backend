d9ecbbb32ab033768b7900c7a8d76995f1b674b8
Saulo Almeida <saulohenrique786@gmail.com>
2022-08-07 17:25:34 -0300

    refactor(test): unit test create

--- dotnet/SpaceFlight.Test/ArticleControllerUnitTest.cs
+++ dotnet/SpaceFlight.Test/ArticleControllerUnitTest.cs
@@ -1,31 +1,30 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Moq;
 using NSubstitute;
 using SpaceFlight.API.Api.Controllers;
 using SpaceFlight.API.Application.DTO.ViewModel;
 using SpaceFlight.API.Application.Model;
 using SpaceFlight.API.Core.Contracts.Infrastructure;
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
 using Xunit;
 
 namespace SpaceFlight.Test
 {
     public class ArticleControllerUnitTest
     {
-        private readonly ArticleController _sut;
-        private readonly IDatabase _db = Substitute.For<IDatabase>();
+        private readonly CancellationToken token = new();
+        private readonly IMongoCollection<Article> mockCollection = Mock.Of<IMongoCollection<Article>>();
 
-        public ArticleControllerUnitTest()
-        {
-            _sut = new ArticleController(_db);
-        }
-
-        [Fact]
-        public async void GetArticleById_ReturnsArticle()
+        [Fact(DisplayName = "Create")]
+        public async void CreateArticle_ReturnsSuccess()
         {
             // Arrange
-            var token = new System.Threading.CancellationToken();
-            var id = 1;
             var dto = new ArticleDTO()
             {
                 ImageUrl = "UNIT.TEST",
@@ -37,15 +36,18 @@ namespace SpaceFlight.Test
                 Url = "UNIT.TEST",
                 Featured = true,
             };
-            Article entity = dto.ToEntity(id);
-            await _db.Collection.InsertOneAsync(entity);
+            var mockContext = new Mock<IContext>();
+            mockContext.Setup(ctx => ctx.Collection).Returns(mockCollection);
+            var sut = new ArticleController(mockContext.Object);
 
             // Act
-            var result = await _sut.GetById(id, token);
-            var httpResult = (OkObjectResult)result;
+            var result = await sut.InsertAsync(dto, token);
+            var httpResult = (CreatedResult)result;
 
             // Assert
-            httpResult.StatusCode.Should().Be(200);
+            httpResult.StatusCode.Should().Be(201);
+            mockContext.Verify(m => m.Collection.InsertOneAsync(It.IsAny<Article>(), null, token), Times.Once());
+            mockContext.Verify(m => m.GetNewIdAsync(token), Times.Once());
         }
     }
 }
