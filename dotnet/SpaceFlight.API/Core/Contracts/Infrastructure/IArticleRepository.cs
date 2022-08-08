f011db7cf7270d84adef73b889ba82ad59ff312b
Saulo Almeida <saulohenrique786@gmail.com>
2022-08-08 19:43:35 -0300

    refactor: add repository contract

---
+++ dotnet/SpaceFlight.API/Core/Contracts/Infrastructure/IArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using SpaceFlight.API.Application.DTO.ViewModel;
+using SpaceFlight.API.Application.Model;
+
+namespace SpaceFlight.API.Core.Contracts.Infrastructure
+{
+    public interface IArticleRepository
+    {
+        Task<IList<Article>> GetAsync(int limit, CancellationToken token);
+        Task<Article> GetByIdAsync(int id, CancellationToken token);
+        Task AddAsync(ArticleDTO dto, CancellationToken token);
+        Task UpdateAsync(int id, ArticleDTO dto, CancellationToken token);
+        Task DeleteAsync(int id, CancellationToken token);
+    }
+}
